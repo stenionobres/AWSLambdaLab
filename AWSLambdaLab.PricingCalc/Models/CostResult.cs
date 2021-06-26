0bdf2b1b850eaa478b4f0148520791ac8fc990c8
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-06-26 17:42:42 -0300

    Creates service to calculate AWS Lambda costs

---
+++ AWSLambdaLab.PricingCalc/Models/CostResult.cs
@@ -0,0 +1,14 @@
+﻿
+namespace AWSLambdaLab.PricingCalc.Models
+{
+    public class CostResult
+    {
+        public int FreeComputeChargeInGigabytePerSecond { get; set; }
+        public int FreeRequestsCharge { get; set; }
+        public decimal ComputePricePerGigabyteSecond { get; set; }
+        public decimal RequestsPricePerMillion { get; set; }
+        public decimal MonthlyComputeCost { get; set; }
+        public decimal MonthlyRequestCost { get; set; }
+        public decimal MonthlyTotalCost { get; set; }
+    }
+}
---
+++ AWSLambdaLab.PricingCalc/Services/ServiceAWSLambdaCost.cs
@@ -0,0 +1,24 @@
+﻿using AWSLambdaLab.PricingCalc.Models;
+using AWSLambdaLab.PricingCalc.Business;
+
+namespace AWSLambdaLab.PricingCalc.Services
+{
+    public class ServiceAWSLambdaCost
+    {
+        public CostResult Calculate(ComputationParameters computationParameters)
+        {
+            var awsLambdaCost = new AWSLambdaCost(computationParameters);
+
+            return new CostResult()
+            {
+                FreeComputeChargeInGigabytePerSecond = awsLambdaCost.FreeTierComputeChargesInGigabytePerSecond,
+                FreeRequestsCharge = awsLambdaCost.FreeTierRequestsChargesInMillions,
+                ComputePricePerGigabyteSecond = awsLambdaCost.ComputePriceByGigabytePerSecond,
+                RequestsPricePerMillion = awsLambdaCost.RequestsPricePerMillion,
+                MonthlyComputeCost = awsLambdaCost.MonthlyComputeCharge,
+                MonthlyRequestCost = awsLambdaCost.MonthlyRequestCharge,
+                MonthlyTotalCost = awsLambdaCost.Calculate()
+            };
+        }
+    }
+}
