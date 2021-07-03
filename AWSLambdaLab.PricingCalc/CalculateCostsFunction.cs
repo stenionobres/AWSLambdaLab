e26d22c6e784dd47d8746f5f5a923369b35a4f27
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-07-03 16:38:11 -0300

    Enable Lambda costs calculate in lambda function

--- AWSLambdaLab.PricingCalc/CalculateCostsFunction.cs
+++ AWSLambdaLab.PricingCalc/CalculateCostsFunction.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using AWSLambdaLab.PricingCalc.Models;
+using AWSLambdaLab.PricingCalc.Services;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -14,18 +15,33 @@ namespace AWSLambdaLab.PricingCalc
     {
         public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            var computationParameters = new ComputationParameters()
+            try
             {
-                AllocatedMemoryByFunctionInMegaBytes = Convert.ToDecimal(request.QueryStringParameters["AllocatedMemoryByFunctionInMegaBytes"]),
-                NumberOfRequests = Convert.ToInt32(request.QueryStringParameters["NumberOfRequests"]),
-                TotalComputeByFunctionInSeconds = Convert.ToDecimal(request.QueryStringParameters["TotalComputeByFunctionInSeconds"])
-            };
+                var computationParameters = new ComputationParameters()
+                {
+                    AllocatedMemoryByFunctionInMegaBytes = Convert.ToDecimal(request.QueryStringParameters["AllocatedMemoryByFunctionInMegaBytes"]),
+                    NumberOfRequests = Convert.ToInt32(request.QueryStringParameters["NumberOfRequests"]),
+                    TotalComputeByFunctionInSeconds = Convert.ToDecimal(request.QueryStringParameters["TotalComputeByFunctionInSeconds"])
+                };
 
-            return new APIGatewayProxyResponse() 
+                var serviceLambdaCost = new ServiceAWSLambdaCost();
+                var costResult = serviceLambdaCost.Calculate(computationParameters);
+
+                return new APIGatewayProxyResponse()
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = JsonConvert.SerializeObject(costResult)
+                };
+            }
+            catch (Exception)
             {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = JsonConvert.SerializeObject(computationParameters)
-            };
+                return new APIGatewayProxyResponse()
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = "Internal Server Error"
+                };
+            }
+            
         }
     }
 }
